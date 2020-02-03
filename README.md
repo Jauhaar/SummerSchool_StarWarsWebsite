b0ffb5b23d0a465f33087b310426f67153d80bf9
Mohammed Jauhaar Saib <m.jauhaar.saib@gmail.com>
2020-02-03 16:26:53 +0200

    Changed fonts of informational text throughout website to improve readability. Added information to the readme.

--- README.md
+++ README.md
@@ -1,1 +1,19 @@
-# Star Wars Website for Immedia Summer School
+# Star Wars Website made for Immedia Summer School
+
+> ### The theme for the website, provided by the Immedia Dev Team, was for me to promote a Star Wars movie.
+
+> ### Features to look out for:
+>
+> - Home Page
+>   - Iconic Star Wars Crawl Text provides some info on the movie
+> - Cast Page
+>   - Hover over images to view info on the actor/actress
+> - Media Gallery Page
+>   - Automated slide show
+>   - Hover over images to view the image in the showcase
+>   - Hover over the slide show to enlargen the showcase
+> - Book Tickets Page
+>   - This page simulates the booking of tickets
+>   - Add and Remove tickets/extras from the shopping cart
+>     - Automated total price calculation
+>   - Click Checkout button with items in the cart for a littel transaction processing animation!
--- js/book-ticket.js
+++ js/book-ticket.js
@@ -4,61 +4,61 @@
 // TODO: add price when hover over extra
 
 // TICKET BOOKING //
-var extrasGrid = document.querySelector(".grid-extras");
-var itemContainer = document.querySelector(".item-container");
-var total = document.querySelector(".total");
-var seatsGrid = document.querySelector(".grid-seats");
-var btnCheckout = document.querySelector(".btn-checkout");
+var extrasGrid = document.querySelector('.grid-extras');
+var itemContainer = document.querySelector('.item-container');
+var total = document.querySelector('.total');
+var seatsGrid = document.querySelector('.grid-seats');
+var btnCheckout = document.querySelector('.btn-checkout');
 
 var tempTotal = 0;
 var checkout = false;
 
 //Add Extras
-extrasGrid.addEventListener("click", function(e) {
+extrasGrid.addEventListener('click', function(e) {
   if (!checkout) {
     if (
-      e.target.classList.contains("popcorn") ||
-      e.target.id == "popcorn-price"
+      e.target.classList.contains('popcorn') ||
+      e.target.id == 'popcorn-price'
     ) {
-      AddItem("popcorn", "popcorn", "25");
+      AddItem('popcorn', 'Popcorn', '25');
       CalcTotal();
     }
-    if (e.target.classList.contains("coke") || e.target.id == "coke-price") {
-      AddItem("coke", "coke", 12);
+    if (e.target.classList.contains('coke') || e.target.id == 'coke-price') {
+      AddItem('coke', 'Coke', 12);
       CalcTotal();
     }
   }
 });
 
 //Remove Item
-itemContainer.addEventListener("click", function(e) {
-  if (e.target.classList.contains("remove")) {
+itemContainer.addEventListener('click', function(e) {
+  if (e.target.classList.contains('remove')) {
     //checks which item was removed then subtracts accordingly
-    if (e.target.parentElement.id == "popcorn") {
-      console.log("POPCOOOOORRRRNNNN");
+    if (e.target.parentElement.id == 'popcorn') {
+      console.log('POPCOOOOORRRRNNNN');
       tempTotal -= 25;
-      var totalPrice = document.querySelector(".total .price");
+      var totalPrice = document.querySelector('.total .price');
       totalPrice.textContent = `R${tempTotal}.00`;
     }
-    if (e.target.parentElement.id == "coke") {
-      console.log("COOOOOOKKKKEEEE");
+    if (e.target.parentElement.id == 'coke') {
+      console.log('COOOOOOKKKKEEEE');
       tempTotal -= 12;
-      var totalPrice = document.querySelector(".total .price");
+      var totalPrice = document.querySelector('.total .price');
       totalPrice.textContent = `R${tempTotal}.00`;
     }
-    if (e.target.parentElement.id == "ticket") {
-      console.log("TIIIIICCCKKKKEEEETTT");
+    if (e.target.parentElement.id == 'ticket') {
+      console.log('TIIIIICCCKKKKEEEETTT');
       tempTotal -= 60;
-      var totalPrice = document.querySelector(".total .price");
+      var totalPrice = document.querySelector('.total .price');
       totalPrice.textContent = `R${tempTotal}.00`;
       //Remove Reserved Seats
-      seats = document.querySelectorAll(".seats");
+      seats = document.querySelectorAll('.seats');
       var found = false;
       var i = 0;
       while (!found) {
-        if (seats[i].classList.contains("reserved-seat")) {
-          seats[i].classList.remove("reserved-seat");
-          seats[i].classList.add("available");
+        if (seats[i].classList.contains('reserved-seat')) {
+          seats[i].classList.remove('reserved-seat');
+          seats[i].classList.add('available');
           found = true;
         }
         i++;
@@ -70,47 +70,47 @@ itemContainer.addEventListener("click", function(e) {
   }
 
   //checks if array is empty then returns R0.00
-  var itemList = document.querySelector(".items-list");
-  var items = itemList.querySelectorAll(".item");
+  var itemList = document.querySelector('.items-list');
+  var items = itemList.querySelectorAll('.item');
   if (items.length == 0) {
     tempTotal = 0;
-    var totalPrice = document.querySelector(".total .price");
+    var totalPrice = document.querySelector('.total .price');
     totalPrice.textContent = `R${tempTotal}.00`;
   }
 });
 
 //Reserve Seat
-seatsGrid.addEventListener("click", function(e) {
+seatsGrid.addEventListener('click', function(e) {
   if (!checkout) {
-    if (e.target.classList.contains("available")) {
-      e.target.classList.add("reserved-seat");
-      e.target.classList.remove("hover");
-      e.target.classList.remove("available");
-      AddItem("ticket", "ticket", "60");
+    if (e.target.classList.contains('available')) {
+      e.target.classList.add('reserved-seat');
+      e.target.classList.remove('hover');
+      e.target.classList.remove('available');
+      AddItem('ticket', 'Ticket', '60');
       CalcTotal();
     }
   }
 });
 
 //Checkout
-btnCheckout.addEventListener("click", function(e) {
-  var itemList = document.querySelector(".items-list");
-  var items = itemList.querySelectorAll(".item");
+btnCheckout.addEventListener('click', function(e) {
+  var itemList = document.querySelector('.items-list');
+  var items = itemList.querySelectorAll('.item');
 
   if (items.length > 0 && !checkout) {
     CheckoutAnimation();
     //Set seats to taken
-    var seatList = document.querySelector(".grid-seats");
-    var seats = seatList.querySelectorAll(".seats");
+    var seatList = document.querySelector('.grid-seats');
+    var seats = seatList.querySelectorAll('.seats');
     Array.from(seats).forEach(function(seat) {
-      if (seat.classList.contains("reserved-seat")) {
-        seat.classList.remove("reserved-seat");
-        seat.classList.add("taken-seat");
+      if (seat.classList.contains('reserved-seat')) {
+        seat.classList.remove('reserved-seat');
+        seat.classList.add('taken-seat');
       }
     });
     //Remove the delete button from the item list
     Array.from(items).forEach(function(item) {
-      temp = item.querySelector(".remove");
+      temp = item.querySelector('.remove');
       temp.remove();
     });
     checkout = true;
@@ -119,26 +119,26 @@ btnCheckout.addEventListener("click", function(e) {
 
 function AddItem(id, item_Name, item_price) {
   //Parent
-  var itemList = document.querySelector(".items-list");
+  var itemList = document.querySelector('.items-list');
 
   //Children
 
-  var li = document.createElement("li");
-  li.className = "item";
+  var li = document.createElement('li');
+  li.className = 'item';
   li.id = id;
 
-  var itemName = document.createElement("span");
-  itemName.className = "item-name";
+  var itemName = document.createElement('span');
+  itemName.className = 'item-name';
 
-  var remove = document.createElement("span");
-  remove.className = "remove";
+  var remove = document.createElement('span');
+  remove.className = 'remove';
 
-  var price = document.createElement("span");
-  price.className = "price";
+  var price = document.createElement('span');
+  price.className = 'price';
 
   //Construct Li
   itemName.appendChild(document.createTextNode(`${item_Name}: `));
-  remove.appendChild(document.createTextNode("x"));
+  remove.appendChild(document.createTextNode('x'));
   price.appendChild(document.createTextNode(`R${item_price}.00`));
   li.appendChild(itemName);
   li.appendChild(remove);
@@ -147,20 +147,20 @@ function AddItem(id, item_Name, item_price) {
 }
 
 function CalcTotal() {
-  var itemList = document.querySelector(".items-list");
-  var items = itemList.querySelectorAll(".item");
+  var itemList = document.querySelector('.items-list');
+  var items = itemList.querySelectorAll('.item');
   var price = 0;
   if (items.length > 0) {
     Array.from(items).forEach(function(item) {
       itemName = item.id;
       switch (itemName) {
-        case "popcorn":
+        case 'popcorn':
           price = 25;
           break;
-        case "coke":
+        case 'coke':
           price = 12;
           break;
-        case "ticket":
+        case 'ticket':
           price = 60;
       }
     });
@@ -170,41 +170,41 @@ function CalcTotal() {
 
   tempTotal += price;
 
-  var totalPrice = document.querySelector(".total .price");
+  var totalPrice = document.querySelector('.total .price');
   totalPrice.textContent = `R${tempTotal}.00`;
 }
 
 function CheckoutAnimation() {
   //Play Checkkout animtion
-  btnCheckout.classList.add("checkout-animation");
+  btnCheckout.classList.add('checkout-animation');
   //Dot animation
-  btnCheckout.firstElementChild.textContent = ".";
+  btnCheckout.firstElementChild.textContent = '.';
   setTimeout(() => {
-    btnCheckout.firstElementChild.textContent = "..";
+    btnCheckout.firstElementChild.textContent = '..';
   }, 500);
   setTimeout(() => {
-    btnCheckout.firstElementChild.textContent = "...";
+    btnCheckout.firstElementChild.textContent = '...';
   }, 1000);
   setTimeout(() => {
-    btnCheckout.firstElementChild.textContent = "....";
+    btnCheckout.firstElementChild.textContent = '....';
   }, 1500);
   setTimeout(() => {
-    btnCheckout.firstElementChild.textContent = "...";
+    btnCheckout.firstElementChild.textContent = '...';
   }, 2000);
   setTimeout(() => {
-    btnCheckout.firstElementChild.textContent = "..";
+    btnCheckout.firstElementChild.textContent = '..';
   }, 2500);
   setTimeout(() => {
-    btnCheckout.firstElementChild.textContent = "...";
+    btnCheckout.firstElementChild.textContent = '...';
   }, 3000);
   setTimeout(() => {
-    btnCheckout.firstElementChild.textContent = "....";
+    btnCheckout.firstElementChild.textContent = '....';
   }, 3500);
   setTimeout(() => {
-    btnCheckout.firstElementChild.textContent = "...";
+    btnCheckout.firstElementChild.textContent = '...';
   }, 4000);
   //Paid
   setTimeout(() => {
-    btnCheckout.firstElementChild.textContent = "paid";
+    btnCheckout.firstElementChild.textContent = 'paid';
   }, 4000);
 }
