b40f89ace350619d81d6d13198eb94fd09ef81ed
Mohammed Jauhaar Saib <m.jauhaar.saib@gmail.com>
2020-02-12 13:09:23 +0200

    Landing page bug fix.

--- js/main.js
+++ js/main.js
@@ -1,4 +1,4 @@
-var special = document.querySelector(".special");
-special.addEventListener("click", function(e) {
-  location.href = "/tickets.html";
+var special = document.querySelector('.special');
+special.addEventListener('click', function(e) {
+  location.href = './tickets.html';
 });
